b4a04bb02b3a647c6c8a087b8d0b46870d3fa288
Patrick Daguio <patrickdaguio@outlook.com>
2021-09-11 09:56:03 +0100

    Added Meta Tags

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -1511,7 +1511,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59922" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65180" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
